56165a6f1b69269097ffae05ac97b49f7e7c0f07
KarlGilden <karl.gildenhuys@gmail.com>
2022-05-04 17:26:19 +1200

    add cors

--- BudgetMateAPI/Startup.cs
+++ BudgetMateAPI/Startup.cs
@@ -29,7 +29,7 @@ namespace BudgetMateAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddDbContext<BudgetMateContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();
             services.AddScoped<IBudgetMateRepo, BudgetMateRepo>();
@@ -48,6 +48,13 @@ namespace BudgetMateAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(options => options
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+            );
 
             app.UseAuthorization();
 
